ea2c7e84777838241be3dc57c0361bddcd62d3c9
Sergio Moreno <sergio.moreno.calzada@everis.com>
2020-02-24 18:35:43 +0100

    fix pod identity resources generation

--- aad-pod-identity.tf
+++ aad-pod-identity.tf
@@ -1,17 +1,21 @@
-resource "local_file" "kubeconfig" {
-  sensitive_content = azurerm_kubernetes_cluster.cloudcommons.kube_admin_config_raw
-  filename = "aad-pod-identity/.kube/config"
-  yml = ""
+locals {
+  yml = var.rbac_enabled ? "aad-pod-identity-rbac.yml" : "aad-pod-identity.yml"
 }
 
-resource "null_resource" "cert_manager" {
-  provisioner "local-exec" {
-    working_dir = "aad-pod-identity/v0.10.1"	
-    command     = "kubectl apply -f crds.yml --kubeconfig ./.kube/config && kubectl apply -f cluster-issuer.yml --kubeconfig ./.kube/config"
+resource "local_file" "kubeconfig" {
+  content = azurerm_kubernetes_cluster.cloudcommons.kube_admin_config_raw
+  filename = "aad-pod-identity/.kube/config"
+}
+
+resource "null_resource" "aad-pod-identity" {
+  count = var.aad_pod_identity_enabled ? 1 : 0
+  provisioner "local-exec" {   
+    working_dir = "."
+    command     = "kubectl apply -f ${local.yml} --kubeconfig ${local_file.kubeconfig.filename}"
     environment = {
-      name           = local.name
-      resource_group = local.resource_group
+      name           = var.name
+      resource_group = var.resource_group
     }
   }
-  depends_on = [module.<%= name %>-kubernetes, local_file.kubeconfig]
+  depends_on = [azurerm_kubernetes_cluster.cloudcommons, local_file.kubeconfig]
 }
